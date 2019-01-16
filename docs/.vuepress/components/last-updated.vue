5bd5b1c7b7f92a9f2fbd59d31ef821b5fb85ee90
Selwyn <talk@selwyn.cc>
2019-01-16 18:28:11 +0100

    Add last updated date to document header
    Use markdown-it plugin to prepend custom Vue component before Vuepress
    parses the markdown components.
    
    Closes #12

---
+++ docs/.vuepress/components/last-updated.vue
@@ -0,0 +1,20 @@
+<template>
+  <p v-if="!isHomePage" class="last-updated">
+    Last updated: <time :datatime="prettyDate">{{prettyDate}}</time>
+  </p>
+</template>
+
+<script>
+  export default {
+    props: ['date', 'isHomePage'],
+    computed: {
+      prettyDate: ({ date }) => date.split('T')[0],
+    },
+  }
+</script>
+
+<style scoped>
+  .last-updated {
+    color: #888;
+  }
+</style>
