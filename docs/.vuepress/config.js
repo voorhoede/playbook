1383b9bd8a751e555741acba78b3e9bc28a30073
Bruna da Fonseca <brunadafonseca@gmail.com>
2019-01-17 16:29:06 +0100

    Add PWA module (#31)
    * feat: add pwa modules and icons
    
    * set plugins options
    
    * fetch manifest
    
    * update manifesta and icons
    
    * remove unused icons
    
    * add updatePopup
    
    * add credentials
    
    * remove update popup
    
    * small changes

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,7 @@
 
 const { readFileSync } = require('fs');
 const generateSidebar = require('../../src/generate-sidebar.js');
+const manifest = require('./public/manifest.json');
 
 module.exports = {
   title: 'Playbook',
@@ -9,5 +10,16 @@ module.exports = {
     sidebar: generateSidebar(JSON.parse(readFileSync('docs/dump.json'))),
   },
   dest: './dist',
+  head: [
+    ['link', { rel: 'icon', href: '/icons/favicon.ico' }],
+    ['link', { rel: 'manifest', href: '/manifest.json', crossorigin: 'use-credentials' }],
+    ['meta', { name: 'theme-color', content: manifest.theme_color }],
+  ],
+  plugins: {
+    '@vuepress/pwa': {
+      serviceWorker: true,
+      updatePopup: false,
+    }
+  },
   evergreen: true,
 };
