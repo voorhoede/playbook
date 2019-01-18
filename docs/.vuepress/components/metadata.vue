9337a18175d5ba316b90810aa1da4a1d0ef3ddf9
Selwyn <talk@selwyn.cc>
2019-01-18 15:11:56 +0100

    Add link to source of document on Dropbox
    Put last updated content in same component to simplify structure and
    avoid duplication of logic.
    
    Closes #11

---
+++ docs/.vuepress/components/metadata.vue
@@ -0,0 +1,43 @@
+<template>
+  <header class="metadata" v-if="!isHomePage">
+    <span class="last-updated">
+      Last updated: <time :datatime="prettyDate">{{prettyDate}}</time>
+    </span>
+    <a
+      class="edit-link"
+      :href="`https://paper.dropbox.com/doc/${id}`"
+      rel="noopener" target="_blank"
+    >
+      Edit on Dropbox
+    </a>
+  </header>
+</template>
+
+<script>
+  export default {
+    props: [
+      'id',
+      'date',
+      'isHomePage',
+    ],
+    computed: {
+      prettyDate: ({ date }) => date.split('T')[0],
+    },
+  }
+</script>
+
+<style scoped>
+  .metadata {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .last-updated,
+  .edit-link {
+    color: #888;
+  }
+
+  .last-updated {
+    margin-right: 1rem;
+  }
+</style>
