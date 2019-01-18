c0d168f9746ad39b72358ab775ea78a432dc4253
bruna <brunadafonseca@gmail.com>
2019-01-18 11:26:54 +0100

    Create component

---
+++ docs/.vuepress/components/last-updated.vue
@@ -0,0 +1,17 @@
+<template>
+  <div v-if="!isHomePage" class="last-updated">
+    <p>Last updated: {{ lastUpdated }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['lastUpdated', 'isHomePage'],
+}
+</script>
+
+<style scoped>
+.last-updated p {
+  font-size: 12px;
+}
+</style>
