fff1788d2125223e9002eeae6830104188128ad7
Selwyn <talk@selwyn.cc>
2019-05-21 16:32:47 +0200

    Revert PWA addition to prevent showing old cached data
    The service worker coming with the Vuepress PWA plugin serves old cached
    data even after hard refreshing a page.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,7 +5,6 @@ const path = require('path');
 
 const documentsMetaData = require('../meta-tree.json');
 const generateSidebar = require('../../src/generate-sidebar.js');
-const manifest = require('./public/manifest.json');
 const urlMatchers = require('../../src/url-matchers.js');
 
 const {
@@ -104,15 +103,7 @@ module.exports = {
   },
   dest: './dist',
   head: [
-    ['link', { rel: 'icon', href: '/icons/favicon.ico' }],
-    ['link', { rel: 'manifest', href: '/manifest.json', crossorigin: 'use-credentials' }],
-    ['meta', { name: 'theme-color', content: manifest.theme_color }],
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
   ],
-  plugins: {
-    '@vuepress/pwa': {
-      serviceWorker: true,
-      updatePopup: false,
-    }
-  },
   evergreen: true,
 };
